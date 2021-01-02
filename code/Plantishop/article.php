02317c4137f84d0110d5adfb0fd60e499b968d99
Astratime <antoine.zaug36@gmail.com>
2021-01-02 16:39:36 +0100

    Update article.php + css

--- code/Plantishop/article.php
+++ code/Plantishop/article.php
@@ -32,15 +32,56 @@
     include '/header.php';
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title><?php echo $getData["nom"] ?></title>
+        <!--<title><?php echo $getData["nom"] ?></title>-->
+        <link rel="stylesheet" href="css/article.css">
     </head>
     <body>
         <script>
             // Cliquer sur le bouton "Ajouter au panier" envoie une requête AJAX à /traitement/ajout_panier.php
         </script>
+
+        <div>
+            <div>
+                <h1>Titre Plante</h1>
+                <h2>Catégorie Plante</h2>
+                <img src="edit.png" width="70px" height="50px" id="edit_button">
+                <img src="trash.png" width="70px" height="50px" id="trash_button">              
+            </div>
+
+            <div>
+
+                <div>
+                    <!-- image de la plante-->
+                    <img src="5.jpg" width="500px" height="500px">
+                </div>
+
+                <div>
+                    <div>
+                        <p>Magnifique plante d'intérieure, le monstera est l'une des plantes
+                            les plus vendues mais aussi l'une des plus résistantes ce qui la rend
+                            facile à cultiver.
+                            Feuillage unique et grand pouvoir décoratif, elle fera votre bonheur.
+                        </p>
+                        <!-- Description article -->
+                    </div>
+
+                    <div>
+                        <p>17,99 €</p>
+                        <!-- Prix -->
+                    </div>
+
+                    <div>
+                        <a href="">Ajouter au panier</a>
+                        <!-- Ajouter au panier -->
+                    </div>
+                </div>
+
+            </div>
+        
+        </div>
     </body>
 </html>
